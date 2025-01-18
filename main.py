49ba06d7b32d51f2bb2d6a6551e81770e1cfaf1d
elbekdavron <elbekd4vronov@gmail.com>
2025-01-18 15:39:43 +0500

    all in one

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ from telegram import Bot
 token = os.getenv("TOKEN")
 
 bot = Bot(token)
-CHAT_ID = "86775091"
-updates = bot.get_updates()
-update = updates[0]
-text = update.message.text
-print(text)
+CHAT_ID = "988528730"
+
+
+file_path = 'audio.ogg'
+response = bot.send_audio(CHAT_ID, file_path)
+print(response)
+
+"""
+file_path = 'audio.mp3'
+response = bot.send_audio(CHAT_ID, file_path)
+print(response)
+
+file_path = 'video.mp4'
+response = bot.send_video(CHAT_ID, file_path)
+print(response)
+
+file_path = 'logo.png'
+response = bot.send_photo(CHAT_ID, file_path)
+print(response)
+
+file_path = 'document.docx'
+response = bot.send_document(CHAT_ID, file_path)
+print(response)
+
+"""
+
--- telegram/bot.py
+++ telegram/bot.py
@@ -32,6 +32,15 @@ class Bot:
             chat_id: The id of the chat to send the photo to
             photo: The path to the photo to send
         """
+        url = self.base_url + "sendPhoto"
+        params = {
+            "chat_id": chat_id
+            }
+        files = {
+            "photo": open(photo, "rb")
+            }
+        response = requests.post(url, params=params, files=files)
+        return response.json()
         
     def send_document(self, chat_id, document):
         """
@@ -41,6 +50,15 @@ class Bot:
             chat_id: The id of the chat to send the document to
             document: The path to the document to send
         """
+        url = self.base_url + "sendDocument"
+        params = {
+            "chat_id": chat_id
+            }
+        files = {
+            "document": open(document, "rb")
+            }
+        response = requests.post(url, params=params, files=files)
+        return response.json()
 
     def send_audio(self, chat_id, audio):
         """
@@ -50,6 +68,15 @@ class Bot:
             chat_id: The id of the chat to send the audio to
             audio: The path to the audio to send
         """
+        url = self.base_url + "sendAudio"
+        params = {
+            "chat_id": chat_id
+            }
+        files = {
+            "audio": open(audio, "rb")
+            }
+        response = requests.post(url, params=params, files=files)
+        return response.json()
 
     def send_video(self, chat_id, video):
         """
@@ -59,6 +86,16 @@ class Bot:
             chat_id: The id of the chat to send the video to
             video: The path to the video to send
         """
+        url = self.base_url + "sendVideo"
+        params = {
+            "chat_id": chat_id
+            }
+        files = {
+            "video": open(video, "rb")
+            }
+        response = requests.post(url, params=params, files=files)
+        return response.json()
+
 
     def send_voice(self, chat_id, voice):
         """
@@ -68,6 +105,11 @@ class Bot:
             chat_id: The id of the chat to send the voice to
             voice: The path to the voice to send
         """
+        url = self.base_url + "sendVoice"
+        params = {"chat_id": chat_id}
+        files = {"voice": open(voice, "rb")}
+        response = requests.post(url, params=params, files=files)
+        return response.json()
 
     def get_updates(self):
         """
@@ -75,5 +117,3 @@ class Bot:
         """
         response = requests.get(self.base_url + "getUpdates")
         return response.json()
-
-    # Add more methods here
